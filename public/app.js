73b17f4d5cf96e64e1de8b2854155661e650ea30
kevinpagtakhan <kevpagtakhan@gmail.com>
2016-11-21 12:09:22 -0800

    Add and test listener

--- public/app.js
+++ public/app.js
@@ -1,13 +1,19 @@
 function createStore(reducer, initialState) {
   let state = initialState;
+  const listeners = [];
 
   const getState = () => (state);
 
   const dispatch = (action) => {
     state = reducer(state, action);
+    listeners.forEach(l => l());
   };
 
+  const subscribe = (listener) => (
+    listeners.push(listener)
+  );
   return {
+    subscribe,
     getState,
     dispatch,
   };
@@ -34,3 +40,13 @@ const initialState = {
 };
 
 const store = createStore(reducer, initialState);
+
+const listener = () => (
+  console.log(store.getState())
+);
+
+store.subscribe(listener);
+
+store.dispatch({type: 'ADD_MESSAGE', message: 'How are you?'});
+store.dispatch({type: 'ADD_MESSAGE', message: 'I am fine. Thank you. How about you?'});
+store.dispatch({type: 'DELETE_MESSAGE', index: 1});
